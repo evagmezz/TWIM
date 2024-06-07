87f33798e499e8c24fb19ede6e029114d3593b35
evagmezz <evag.251103@gmail.com>
2024-06-07 17:15:17 +0200

    cargan los datos al lanzar el docker

--- database/twim.js
+++ database/twim.js
@@ -9,4 +9,378 @@ db.createUser({
   ],
 })
 
-db = db.getSiblingDB('mongoTwim')
+db = db.getSiblingDB('mongoTwim');
+
+db.comments.insertMany([
+  {
+    _id: "766c443e-ef80-418d-b249-2f3439992780",
+    __v: 0,
+    content: "que guapos!!!!",
+    createdAt: new Date("2024-04-25T09:16:10.196Z"),
+    postId: "d08939f3-edcd-40c9-8349-9c290409c8b1",
+    updatedAt: new Date("2024-04-25T09:16:10.196Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "6c8be092-5127-443c-84a4-ee9e94090840",
+    __v: 0,
+    content: "te voy a echar mucho de menos :(",
+    createdAt: new Date("2024-04-25T09:16:22.155Z"),
+    postId: "90512b25-ffd7-4f91-9333-72ab843514b2",
+    updatedAt: new Date("2024-04-25T09:16:22.155Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "e5191b07-9fe9-403d-bc8d-efa93a51d5d8",
+    __v: 0,
+    content: "TE ECHO DE MENOS",
+    createdAt: new Date("2024-04-25T09:16:39.616Z"),
+    postId: "f07a44ae-e710-4dbd-abc9-164093adc263",
+    updatedAt: new Date("2024-04-25T09:16:39.616Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "6df33675-0e45-4f85-9a66-c05bbe4b304b",
+    __v: 0,
+    content: "que bonito!!!",
+    createdAt: new Date("2024-04-25T10:23:15.432Z"),
+    postId: "2852e84f-32d6-4204-b632-aa37972ddc56",
+    updatedAt: new Date("2024-04-25T10:23:15.432Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "2d39cae3-b711-4a25-b3ca-2dc85749097d",
+    __v: 0,
+    content: "QUE GUAPAAS",
+    createdAt: new Date("2024-04-25T10:23:27.601Z"),
+    postId: "09840bc1-ca0f-4d2f-9a7e-89329c3aea14",
+    updatedAt: new Date("2024-04-25T10:23:27.601Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "c92cd2f5-7fb7-498b-9d40-a225046a8e8a",
+    __v: 0,
+    content: "somos las mejores!",
+    createdAt: new Date("2024-04-25T10:23:35.178Z"),
+    postId: "df4ca0ea-73f3-4c70-b695-bebe42833e2f",
+    updatedAt: new Date("2024-04-25T10:23:35.178Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "0c9092b3-9adb-4ee2-b8e9-05d369990601",
+    __v: 0,
+    content: "guapisimas;)",
+    createdAt: new Date("2024-04-25T10:23:50.513Z"),
+    postId: "cc51ecb2-9be7-4316-986b-b30df89cda09",
+    updatedAt: new Date("2024-04-25T10:23:50.513Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "5349b536-bf87-48df-a507-c8f79a98c010",
+    __v: 0,
+    content: "que lindaaas",
+    createdAt: new Date("2024-04-25T11:13:19.241Z"),
+    postId: "18ec1e26-32ae-4b8f-9457-5d8a0700659a",
+    updatedAt: new Date("2024-04-25T11:13:19.241Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "f09dd7f8-ce48-4233-8e05-46bd836d906c",
+    __v: 0,
+    content: "vamos juntas!!!",
+    createdAt: new Date("2024-04-25T12:12:10.562Z"),
+    postId: "e1633d35-d357-4c4c-ac8b-768f9353e92c",
+    updatedAt: new Date("2024-04-25T12:12:10.562Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "2684cba0-9d84-4517-bdf8-6b5dde9ff63a",
+    __v: 0,
+    content: "xoxoooo que reinaaaa",
+    createdAt: new Date("2024-04-26T12:28:11.610Z"),
+    postId: "86a11f3f-45a4-4e14-8d57-fd1e8d1599c3",
+    updatedAt: new Date("2024-04-26T12:28:11.610Z"),
+    userId: "7ec16af6-c69e-4883-8698-836c9bc2cda9"
+  },
+  {
+    _id: "53a65d3c-6e53-4acf-81b7-716daeaac528",
+    __v: 0,
+    content: "lulu la mejor",
+    createdAt: new Date("2024-04-26T12:50:15.881Z"),
+    postId: "df4ca0ea-73f3-4c70-b695-bebe42833e2f",
+    updatedAt: new Date("2024-04-26T12:50:15.881Z"),
+    userId: "f280aaea-04b2-42cd-b0c1-bcba503d88c9"
+  },
+  {
+    _id: "4b2fe48d-ff27-46aa-ae6d-805b422cf0f7",
+    __v: 0,
+    content: "que guapas!!",
+    createdAt: new Date("2024-04-26T12:50:39.473Z"),
+    postId: "1095bade-6db2-4036-b32e-dd56d8e9830b",
+    updatedAt: new Date("2024-04-26T12:50:39.473Z"),
+    userId: "f280aaea-04b2-42cd-b0c1-bcba503d88c9"
+  },
+  {
+    _id: "53745cfd-8c1c-46bd-940d-b3988784b5a9",
+    __v: 0,
+    content: "Vaya par de guapas!!!!!",
+    createdAt: new Date("2024-04-28T09:57:59.239Z"),
+    postId: "05ccc5ab-224b-469d-b317-6a0079407e4f",
+    updatedAt: new Date("2024-04-28T09:57:59.239Z"),
+    userId: "f280aaea-04b2-42cd-b0c1-bcba503d88c9"
+  },
+  {
+    _id: "42c6428c-3773-414f-b5c9-6955982b80ff",
+    __v: 0,
+    content: "toma que toma!!!",
+    createdAt: new Date("2024-04-28T09:59:00.164Z"),
+    postId: "51e46287-78e4-4eb2-93c1-958999044bec",
+    updatedAt: new Date("2024-04-28T09:59:00.164Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "6eb21139-e633-47f4-8f16-e32782efc151",
+    __v: 0,
+    content: "que guapoooos!🩷",
+    createdAt: new Date("2024-04-28T09:59:34.866Z"),
+    postId: "96ce85d8-df42-4cca-a4b1-54db0c4b888d",
+    updatedAt: new Date("2024-04-28T09:59:34.866Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "e716ae1a-38e1-4f2f-8808-12c8ea998723",
+    __v: 0,
+    content: "que envidiaaaa😭",
+    createdAt: new Date("2024-04-28T10:00:35.704Z"),
+    postId: "ac69cb0d-c661-480d-9f84-88047f572820",
+    updatedAt: new Date("2024-04-28T10:00:35.704Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "2da8e9bb-95d1-40a3-a5f9-b5822ef9ed6a",
+    __v: 0,
+    content: "que pikete",
+    createdAt: new Date("2024-04-28T10:00:45.028Z"),
+    postId: "b351f4ba-f545-4f00-896d-fe3f0a2b1018",
+    updatedAt: new Date("2024-04-28T10:00:45.028Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "2f8b0ac3-a246-4b1f-b0d1-741e4f72268d",
+    __v: 0,
+    content: "que chuleriaaaaa!!",
+    createdAt: new Date("2024-04-28T10:01:23.557Z"),
+    postId: "96ce85d8-df42-4cca-a4b1-54db0c4b888d",
+    updatedAt: new Date("2024-04-28T10:01:23.557Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "ba8adcf0-3810-4850-9e57-e4a75cba6426",
+    __v: 0,
+    content: "una preguntita",
+    createdAt: new Date("2024-04-28T10:01:37.566Z"),
+    postId: "33f6ea1b-eb5b-43b2-9e8c-0c5fb8026b1f",
+    updatedAt: new Date("2024-04-28T10:01:37.566Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "e71aadec-7b01-4ca0-9084-0669233b7207",
+    __v: 0,
+    content: "guapaa",
+    createdAt: new Date("2024-05-22T17:00:24.321Z"),
+    postId: "a25e7b1f-0c38-4487-85c4-da68e353d31f",
+    updatedAt: new Date("2024-05-22T17:00:24.321Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "2c4244cd-300a-4bae-b6c6-0aaafe20d62c",
+    __v: 0,
+    content: "soy muy egocentrica",
+    createdAt: new Date("2024-05-24T07:33:20.096Z"),
+    postId: "09840bc1-ca0f-4d2f-9a7e-89329c3aea14",
+    updatedAt: new Date("2024-05-24T07:33:20.096Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  }
+]);
+
+
+db.posts.insertMany([
+  {
+    _id: "90512b25-ffd7-4f91-9333-72ab843514b2",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:00:42.424Z"),
+    likes: ["eab57d50-b523-479b-a20e-fa9a14ae201f", "f280aaea-04b2-42cd-b0c1-bcba503d88c9"],
+    location: "Aeropuerto Barajas de Madrid",
+    photos: [
+      "http://localhost:3000/photos/1714035642393_image1.jpeg",
+      "http://localhost:3000/photos/1714035642397_image0.jpeg"
+    ],
+    title: "Después de un largo dia de viaje, empieza mi nueva vida!!",
+    updatedAt: new Date("2024-05-22T10:09:58.731Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "d08939f3-edcd-40c9-8349-9c290409c8b1",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:03:36.938Z"),
+    likes: ["eab57d50-b523-479b-a20e-fa9a14ae201f", "ad50696b-84e7-48ef-acff-c55e51555444"],
+    location: "Amsterdam",
+    photos: [
+      "http://localhost:3000/photos/1714035816916_image0(1).jpeg",
+      "http://localhost:3000/photos/1714035816920_image4.jpeg",
+      "http://localhost:3000/photos/1714035816924_image3.jpeg",
+      "http://localhost:3000/photos/1714035816928_image2.jpeg",
+      "http://localhost:3000/photos/1714035816933_image1(1).jpeg",
+      "http://localhost:3000/photos/1714035816935_5e18a8a3-8bf8-443e-8187-12f77f3b7595.jpg"
+    ],
+    title: "Boat trip... chu chu!",
+    updatedAt: new Date("2024-05-22T09:52:36.034Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "86a11f3f-45a4-4e14-8d57-fd1e8d1599c3",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:04:50.247Z"),
+    likes: ["eab57d50-b523-479b-a20e-fa9a14ae201f", "7ec16af6-c69e-4883-8698-836c9bc2cda9"],
+    location: "Madrid",
+    photos: [
+      "http://localhost:3000/photos/1714035890241_image1(2).jpeg",
+      "http://localhost:3000/photos/1714035890241_image0(2).jpeg"
+    ],
+    title: "Beautiful sunset",
+    updatedAt: new Date("2024-05-22T09:52:36.036Z"),
+    userId: "ad50696b-84e7-48ef-acff-c55e51555444"
+  },
+  {
+    _id: "2852e84f-32d6-4204-b632-aa37972ddc56",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:17:53.446Z"),
+    likes: ["ad50696b-84e7-48ef-acff-c55e51555444", "7ec16af6-c69e-4883-8698-836c9bc2cda9", "eab57d50-b523-479b-a20e-fa9a14ae201f"],
+    location: "Bruselas, Bélgica",
+    photos: [
+      "http://localhost:3000/photos/1710865285876_Capturadepantalla2024-03-19171944.png",
+      "http://localhost:3000/photos/1710866077924_Capturadepantalla2024-03-19171921.png",
+      "http://localhost:3000/photos/1711971233506_Capturadepantalla2024-03-19171956.png",
+      "http://localhost:3000/photos/1711971608691_Capturadepantalla2024-03-19171935.png",
+      "http://localhost:3000/photos/1711893328215_Capturadepantalla2024-03-19172029.png"
+    ],
+    title: "Bruselas es precioso",
+    updatedAt: new Date("2024-05-22T09:52:36.038Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "89674a84-0906-4355-932a-076fa696a463",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:21:51.363Z"),
+    likes: ["ad50696b-84e7-48ef-acff-c55e51555444", "7ec16af6-c69e-4883-8698-836c9bc2cda9", "eab57d50-b523-479b-a20e-fa9a14ae201f"],
+    location: "Cantabria, España",
+    photos: [
+      "http://localhost:3000/photos/1710865376676_Capturadepantalla2024-03-19171808.png",
+      "http://localhost:3000/photos/1710865376686_Capturadepantalla2024-03-19171825.png",
+      "http://localhost:3000/photos/1710865469561_4558BAF8-F296-466C-B1A6-F75F98BF552E.jpeg",
+      "http://localhost:3000/photos/1710865469642_Capturadepantalla2024-03-19171757.png",
+      "http://localhost:3000/photos/1710937690986_B55EE6AD-47A2-487F-8467-56B60CD30DF8.jpeg",
+      "http://localhost:3000/photos/1710937691116_Capturadepantalla2024-03-19171852.png",
+      "http://localhost:3000/photos/1710937708843_Capturadepantalla2024-03-19171550.png"
+    ],
+    title: "Ir al norte es terapéutico",
+    updatedAt: new Date("2024-05-22T17:06:52.662Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "1095bade-6db2-4036-b32e-dd56d8e9830b",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:30:47.922Z"),
+    likes: ["ad50696b-84e7-48ef-acff-c55e51555444", "f280aaea-04b2-42cd-b0c1-bcba503d88c9"],
+    location: "Lisboa, Portugal",
+    photos: [
+      "http://localhost:3000/photos/1714037447845_IMG_8768.jpg",
+      "http://localhost:3000/photos/1714037447854_IMG_6846.jpg",
+      "http://localhost:3000/photos/1714037447864_IMG_8781.jpg",
+      "http://localhost:3000/photos/1714037447875_IMG_8866.jpg",
+      "http://localhost:3000/photos/1714037447893_IMG_8879.jpg",
+      "http://localhost:3000/photos/1714037447904_IMG_7001.jpg",
+      "http://localhost:3000/photos/1714037447915_961B7F35-7AB0-481C-BE7E-FF3199D54540.jpg"
+    ],
+    title: "Portugal y ellas",
+    updatedAt: new Date("2024-05-22T09:52:36.042Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "09840bc1-ca0f-4d2f-9a7e-89329c3aea14",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:34:38.426Z"),
+    likes: ["eab57d50-b523-479b-a20e-fa9a14ae201f", "7ec16af6-c69e-4883-8698-836c9bc2cda9"],
+    location: "Prague, Czechia",
+    photos: [
+      "http://localhost:3000/photos/1714037678501_IMG_2019.jpg",
+      "http://localhost:3000/photos/1714037678511_IMG_2002.jpg",
+      "http://localhost:3000/photos/1714037678521_IMG_2001.jpg",
+      "http://localhost:3000/photos/1714037678522_IMG_1997.jpg",
+      "http://localhost:3000/photos/1714037678530_IMG_1993.jpg"
+    ],
+    title: "Museo Kafka",
+    updatedAt: new Date("2024-05-22T09:52:36.044Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "88e4a8d5-0e38-48b3-a8f5-2221f00447a6",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:38:30.931Z"),
+    likes: ["eab57d50-b523-479b-a20e-fa9a14ae201f", "7ec16af6-c69e-4883-8698-836c9bc2cda9"],
+    location: "Madrid, España",
+    photos: [
+      "http://localhost:3000/photos/1710859791214_IMG_6628.jpg",
+      "http://localhost:3000/photos/1710859791223_IMG_7063.jpg",
+      "http://localhost:3000/photos/1710859791231_IMG_6576.jpg",
+      "http://localhost:3000/photos/1710859791241_IMG_6775.jpg"
+    ],
+    title: "Volver",
+    updatedAt: new Date("2024-05-22T09:52:36.045Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "52076c01-2d14-4b25-82d5-2c0f5e0c2545",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:42:00.264Z"),
+    likes: ["eab57d50-b523-479b-a20e-fa9a14ae201f", "f280aaea-04b2-42cd-b0c1-bcba503d88c9"],
+    location: "Barcelona, España",
+    photos: [
+      "http://localhost:3000/photos/1710865642415_Capturadepantalla2024-03-19171719.png",
+      "http://localhost:3000/photos/1710865642415_IMG_0014.JPG",
+      "http://localhost:3000/photos/1710865642423_Capturadepantalla2024-03-19171732.png",
+      "http://localhost:3000/photos/1710865642430_IMG_0015.JPG",
+      "http://localhost:3000/photos/1710865642438_IMG_0017.JPG"
+    ],
+    title: "Una tarde en el parque",
+    updatedAt: new Date("2024-05-22T09:52:36.047Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  },
+  {
+    _id: "7da22c28-2a4b-44a2-8132-4b42acb28a2a",
+    __v: 0,
+    comments: [],
+    createdAt: new Date("2024-04-25T09:45:01.192Z"),
+    likes: ["eab57d50-b523-479b-a20e-fa9a14ae201f", "f280aaea-04b2-42cd-b0c1-bcba503d88c9", "7ec16af6-c69e-4883-8698-836c9bc2cda9"],
+    location: "Budapest, Hungría",
+    photos: [
+      "http://localhost:3000/photos/1710865325106_Capturadepantalla2024-03-19171840.png",
+      "http://localhost:3000/photos/1710865325106_IMG_0013.JPG",
+      "http://localhost:3000/photos/1710865325114_IMG_0012.JPG",
+      "http://localhost:3000/photos/1710865325121_Capturadepantalla2024-03-19171858.png"
+    ],
+    title: "Hungría y sus paisajes",
+    updatedAt: new Date("2024-05-22T09:52:36.049Z"),
+    userId: "eab57d50-b523-479b-a20e-fa9a14ae201f"
+  }
+]);
+
